d035dccd608c6cd9725f8375a9f8ccc006517632
jetpackpony <jetpackpony@gmail.com>
2018-11-09 18:54:56 +0300

    Add a redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
 
 const persistConfig = {
   key: 'root',
@@ -12,7 +13,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, reducer);
 
 const configureStore = () => {
-  const store = createStore(persistedReducer);
+  const store = createStore(persistedReducer, applyMiddleware(logger));
   const persistor = persistStore(store);
   return { store, persistor };
 };
