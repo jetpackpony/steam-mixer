7f5dd2df954c52402a132f684fb720fe15789708
jetpackpony <jetpackpony@gmail.com>
2018-11-09 19:30:02 +0300

    Move all the handlers from audioGraph reducer into handlers module

--- src/store/reducers/audioGraph/handlers.js
+++ src/store/reducers/audioGraph/handlers.js
@@ -1,5 +1,58 @@
 import * as R from 'ramda';
+import { NODE_TYPES } from '../../constants';
 import { getNodeIndexByID } from './utils';
+
+export const addEndpoint = (state, action) => {
+  return [
+    ...state,
+    {
+      "nodeId": action.nodeId,
+      "title": action.title,
+      "type": action.deviceType,
+      "output": [],
+      "deviceId": action.device.deviceId
+    }
+  ];
+};
+
+export const addGainNode = (state, action) => {
+  return [
+    ...state,
+    {
+      "nodeId": action.nodeId,
+      "title": action.title,
+      "type": NODE_TYPES.AUDIONODE,
+      "output": [],
+      "constructor": "gain",
+      "props": {
+        "gain": 1
+      }
+    }
+  ];
+};
+
+export const addConnection = (state, action) => {
+  var fromIndex = getNodeIndexByID(action.fromId, state);
+  return [
+    ...R.slice(0, fromIndex, state),
+    {
+      ...state[fromIndex],
+      output: R.uniq([
+        ...state[fromIndex].output,
+        action.toId
+      ])
+    },
+    ...R.slice(fromIndex + 1, Infinity, state),
+  ];
+};
+
+export const deleteNode = (state, action) => {
+  var nodeIndex = getNodeIndexByID(action.nodeId, state);
+  return R.map((node) => ({
+    ...node,
+    output: R.without([action.nodeId], node.output)
+  }), R.remove(nodeIndex, 1, state));
+};
 
 export const deleteConnection = (state, action) => {
   const fromIndex = getNodeIndexByID(action.fromId, state);
@@ -10,5 +63,20 @@ export const deleteConnection = (state, action) => {
       output: R.without([action.toId], state[fromIndex].output)
     },
     ...R.slice(fromIndex + 1, Infinity, state),
+  ];
+};
+
+export const changeGain = (state, action) => {
+  var nodeIndex = getNodeIndexByID(action.nodeId, state);
+  return [
+    ...R.slice(0, nodeIndex, state),
+    {
+      ...state[nodeIndex],
+      props: {
+        ...state[nodeIndex].props,
+        gain: action.value
+      }
+    },
+    ...R.slice(nodeIndex + 1, Infinity, state),
   ];
 };
--- src/store/reducers/audioGraph/index.js
+++ src/store/reducers/audioGraph/index.js
@@ -1,74 +1,27 @@
 import * as R from 'ramda';
-import { NODE_TYPES } from '../../constants';
 import { ACTION_TYPES } from '../../actions';
 import initState from './initialState';
-import { deleteConnection } from './handlers';
-import { getNodeIndexByID } from './utils';
+import {
+  addEndpoint, addGainNode, addConnection,
+  deleteNode, deleteConnection, changeGain
+} from './handlers';
+
+const handlersMap = {
+  [ACTION_TYPES.ADD_ENDPOINT]: addEndpoint,
+  [ACTION_TYPES.ADD_GAIN_NODE]: addGainNode,
+  [ACTION_TYPES.ADD_CONNECTION]: addConnection,
+  [ACTION_TYPES.DELETE_NODE]: deleteNode,
+  [ACTION_TYPES.DELETE_CONNECTION]: deleteConnection,
+  [ACTION_TYPES.CHANGE_GAIN]: changeGain
+}
+
+const stateIdentity = (state) => state;
+const getHandler = (actionType) => (
+  handlersMap[actionType] || stateIdentity
+);
 
 const audioGraph = (state = initState, action) => {
-  switch (action.type) {
-    case ACTION_TYPES.ADD_ENDPOINT:
-      return [
-        ...state,
-        {
-          "nodeId": action.nodeId,
-          "title": action.title,
-          "type": action.deviceType,
-          "output": [],
-          "deviceId": action.device.deviceId
-        }
-      ];
-    case ACTION_TYPES.ADD_GAIN_NODE:
-      return [
-        ...state,
-        {
-          "nodeId": action.nodeId,
-          "title": action.title,
-          "type": NODE_TYPES.AUDIONODE,
-          "output": [],
-          "constructor": "gain",
-          "props": {
-            "gain": 1
-          }
-        }
-      ];
-    case ACTION_TYPES.ADD_CONNECTION:
-      var fromIndex = getNodeIndexByID(action.fromId, state);
-      return [
-        ...R.slice(0, fromIndex, state),
-        {
-          ...state[fromIndex],
-          output: R.uniq([
-            ...state[fromIndex].output,
-            action.toId
-          ])
-        },
-        ...R.slice(fromIndex + 1, Infinity, state),
-      ];
-    case ACTION_TYPES.DELETE_NODE:
-      var nodeIndex = getNodeIndexByID(action.nodeId, state);
-      return R.map((node) => ({
-        ...node,
-        output: R.without([action.nodeId], node.output)
-      }), R.remove(nodeIndex, 1, state));
-    case ACTION_TYPES.DELETE_CONNECTION:
-      return deleteConnection(state, action);
-    case ACTION_TYPES.CHANGE_GAIN:
-      var nodeIndex = getNodeIndexByID(action.nodeId, state);
-      return [
-        ...R.slice(0, nodeIndex, state),
-        {
-          ...state[nodeIndex],
-          props: {
-            ...state[nodeIndex].props,
-            gain: action.value
-          }
-        },
-        ...R.slice(nodeIndex + 1, Infinity, state),
-      ];
-    default:
-      return state;
-  }
+  return getHandler(action.type)(state, action);
 };
 
 export default audioGraph;
