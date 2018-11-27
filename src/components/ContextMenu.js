4498cb6e3d736dcf6e59af4ab2219b9fcce8357c
jetpackpony <jetpackpony@gmail.com>
2018-11-27 15:21:07 +0300

    Move context menu's styles into an scss file

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './ContextMenu.module.scss';
 
 const ContextMenu = ({ coords, actions, isOpen, toggle }) => {
   return (
     <div
+      className={styles.menu}
       style={{
-        ...menuStyle,
         left: coords.x,
         top: coords.y,
         display: (isOpen) ? "block" : "none"
@@ -15,8 +16,8 @@ const ContextMenu = ({ coords, actions, isOpen, toggle }) => {
         actions.map(({ title, onClick }) => (
           <button
             key={title}
+            className={styles.item}
             type="button"
-            style={itemStyle}
             onClick={onClick}
           >
             {title}
@@ -25,34 +26,6 @@ const ContextMenu = ({ coords, actions, isOpen, toggle }) => {
       }
     </div>
   );
-};
-
-const menuStyle = {
-  display: "block",
-  position: "absolute",
-  top: "100 %",
-  left: "0",
-  zIndex: "1000",
-  minWidth: "10rem",
-  padding: "0.5rem 0",
-  margin: "0.125rem 0 0",
-  color: "#fff",
-  textAlign: "left",
-  backgroundColor: "#222",
-  backgroundClip: "padding-box",
-  border: "1px solid #444",
-  borderRadius: "0.25rem"
-};
-
-const itemStyle = {
-  display: "block",
-  width: "100%",
-  padding: ".25rem 1.5rem",
-  textAlign: "inherit",
-  backgroundColor: "transparent",
-  border: "0",
-  color: "#d9d7d3",
-  cursor: "pointer"
 };
 
 export default ContextMenu;
