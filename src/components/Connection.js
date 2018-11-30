088a7e22d9784e0e75528d5f0061023e584f1a4d
jetpackpony <jetpackpony@gmail.com>
2018-11-30 19:00:25 +0300

    Add "ports" to nodes on canvas and connect connections to them

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { Line } from 'react-konva';
+import { Line, Group, Circle } from 'react-konva';
 import withContextMenu from './withContextMenu';
 import { withTheme } from '@material-ui/core/styles';
+import { getPortCoords } from './Node';
 
 const Connection = ({ fromCoords, toCoords, onClick, theme }) => {
-  const from = [fromCoords.x, fromCoords.y];
-  const to = [toCoords.x, toCoords.y];
-  const controls = [fromCoords.x + 100, fromCoords.y, toCoords.x - 100, toCoords.y];
+  const fromPortCoords = getPortCoords(fromCoords).output;
+  const toPortCoords = getPortCoords(toCoords).input;
+  const from = [fromPortCoords.x, fromPortCoords.y];
+  const to = [toPortCoords.x, toPortCoords.y];
+  const controls = [from[0] + 100, from[1], to[0] - 100, to[1]];
   const points = Array.prototype.concat(from, controls, to);
+  const lineColor = theme.palette.grey[600];
   return (
-    <Line
-      points={points}
-      stroke={theme.palette.grey[600]}
-      strokeWidth="2"
-      onClick={onClick}
-      lineCap="round"
-      lineJoin="round"
-      bezier={true}
-    />
+    <Group onClick={onClick}>
+      <Circle
+        x={fromPortCoords.x}
+        y={fromPortCoords.y}
+        fill={lineColor}
+        radius={3}
+        strokeEnabled={false}
+      />
+      <Circle
+        x={toPortCoords.x}
+        y={toPortCoords.y}
+        fill={lineColor}
+        radius={3}
+        strokeEnabled={false}
+      />
+      <Line
+        points={points}
+        stroke={lineColor}
+        strokeWidth="2"
+        lineCap="round"
+        lineJoin="round"
+        bezier={true}
+      />
+      {
+        // The second line is here for easier clicability
+      }
+      <Line
+        points={points}
+        stroke="transparent"
+        strokeWidth="10"
+        lineCap="round"
+        lineJoin="round"
+        bezier={true}
+      />
+    </Group>
   );
 };
 
--- src/components/Node.js
+++ src/components/Node.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import { Circle, Text, Group } from 'react-konva';
+import * as R from 'ramda';
+import { Circle, Text, Group, Arc } from 'react-konva';
 import withContextMenu from './withContextMenu';
 import { withTheme } from '@material-ui/core/styles';
+import { NODE_TYPES } from '../store/constants';
+
+const circleRadius = 28;
+const iconSize = 24;
+const labelWidth = circleRadius * 4;
+const labelMargin = 10;
+const portMargin = 10;
+const portRadius = 5;
+const portWidth = 2;
 
 const getNodeIconName = (nodeType) => (
   {
-    SOURCE: "mic",
-    DESTINATION: "headset",
-    AUDIONODE: "equalizer"
+    [NODE_TYPES.SOURCE]: "mic",
+    [NODE_TYPES.DESTINATION]: "headset",
+    [NODE_TYPES.AUDIONODE]: "equalizer"
   }[nodeType]
+);
+
+export const getPortCoords = ({ x, y }) => ({
+  input: {
+    x: x - (circleRadius + portMargin),
+    y: y 
+  },
+  output: {
+    x: x + circleRadius + portMargin,
+    y: y
+  }
+});
+
+const hasInputs = (type) => (
+  R.contains(type, [NODE_TYPES.AUDIONODE, NODE_TYPES.DESTINATION])
+);
+
+const hasOutputs = (type) => (
+  R.contains(type, [NODE_TYPES.AUDIONODE, NODE_TYPES.SOURCE])
 );
 
 const Node = ({ nodeId, nodeType, title, coords, onClick, onMove, theme }) => {
   const secondaryColor = theme.palette.secondary[theme.palette.type];
   const fontColor = theme.typography.body1.color;
-  const circleRadius = 28;
-  const iconSize = 24;
-  const labelWidth = circleRadius * 4;
-  const labelMargin = 10;
   const iconName = getNodeIconName(nodeType);
+  const portCoords = getPortCoords({ x: 0, y: 0 });
 
+  console.log(theme);
   return (
       <Group
         x={coords.x}
@@ -44,6 +71,38 @@ const Node = ({ nodeId, nodeType, title, coords, onClick, onMove, theme }) => {
           shadowBlur="5"
           shadowOffset={{ x: 0, y: 3 }}
         />
+        {
+          (hasInputs(nodeType))
+            ? <Arc
+              x={portCoords.input.x}
+              y={portCoords.input.y}
+              angle={160}
+              innerRadius={portRadius}
+              outerRadius={portRadius + portWidth}
+              fill={secondaryColor}
+              strokeEnabled={false}
+              lineCap="round"
+              lineJoin="round"
+              rotation={-80}
+            />
+            : null
+        }
+        {
+          (hasOutputs(nodeType))
+            ? <Arc
+              x={portCoords.output.x}
+              y={portCoords.output.y}
+              angle={160}
+              innerRadius={portRadius}
+              outerRadius={portRadius + portWidth}
+              fill={secondaryColor}
+              strokeEnabled={false}
+              lineCap="round"
+              lineJoin="round"
+              rotation={100}
+            />
+            : null
+        }
         <Text
           x={-circleRadius}
           y={-circleRadius}
