f9fb88d88b964da90060b9f24ad49904caf3b63f
jetpackpony <jetpackpony@gmail.com>
2018-11-30 15:20:30 +0300

    Add styling to node on canvas

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Rect, Text, Group } from 'react-konva';
+import { Circle, Text, Group } from 'react-konva';
 import withContextMenu from './withContextMenu';
 import { withTheme } from '@material-ui/core/styles';
 
 const Node = ({ nodeId, title, coords, onClick, onMove, theme }) => {
+  const secondaryColor = theme.palette.secondary[theme.palette.type];
+  const fontColor = theme.typography.body1.color;
+  const circleRadius = 28;
+  const iconSize = 24;
+  const labelWidth = circleRadius * 4;
+  const labelMargin = 10;
+
   return (
       <Group
         x={coords.x}
@@ -18,21 +25,37 @@ const Node = ({ nodeId, title, coords, onClick, onMove, theme }) => {
           onMove(nodeId, coords);
         }}
       >
-        <Rect
-          x={10}
+        <Circle
+          x={0}
           y={0}
-          width={30}
-          height={30}
-          fill="red"
-          shadowBlur={3}
+          fill={secondaryColor}
+          radius={circleRadius}
+          strokeEnabled={false}
+          shadowColor="rgba(0, 0, 0, 0.2)"
+          shadowBlur="5"
+          shadowOffset={{ x: 0, y: 3 }}
+        />
+        <Text
+          x={-circleRadius}
+          y={-circleRadius}
+          width={circleRadius * 2}
+          height={circleRadius * 2}
+          text="mic"
+          fill={fontColor}
+          fontFamily="Material Icons"
+          fontSize={iconSize}
+          align="center"
+          verticalAlign="middle"
         />
         <Text
           text={title}
-          fill={theme.typography.body1.color}
+          fill={fontColor}
           fontFamily={theme.typography.fontFamily}
           fontSize={theme.typography.fontSize}
-          x={0}
-          y={40}
+          x={-labelWidth / 2}
+          y={circleRadius + labelMargin}
+          width={labelWidth}
+          align="center"
         />
       </Group>
   );
