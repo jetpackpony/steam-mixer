74055b11e8f133dab75219ceb3c757a7a153274b
jetpackpony <jetpackpony@gmail.com>
2018-12-04 16:34:48 +0300

    Use proper icons in the menu

--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -2,25 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Menu from './Menu';
 import {
-  toggleAddInputModal, toggleAddAudioNodeModal, toggleAddOutputModal
+  toggleAddInputModal,
+  toggleAddAudioNodeModal,
+  toggleAddOutputModal
 } from '../store/actions';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
+import Mic from '@material-ui/icons/Mic';
+import Headset from '@material-ui/icons/Headset';
+import Equalizer from '@material-ui/icons/Equalizer';
 
 const mapDispatch = (dispatch) => ({
   actions: [
     {
       title: "Add Input",
-      icon: <InboxIcon />,
+      icon: <Mic/>,
       onClick: () => dispatch(toggleAddInputModal())
     },
     {
       title: "Add Output",
-      icon: <InboxIcon />,
+      icon: <Headset/>,
       onClick: () => dispatch(toggleAddOutputModal())
     },
     {
       title: "Add Audio Node",
-      icon: <InboxIcon />,
+      icon: <Equalizer/>,
       onClick: () => dispatch(toggleAddAudioNodeModal())
     }
   ]
