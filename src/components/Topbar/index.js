5c2b3c877cb412e72df66191f68075b04e36b478
jetpackpony <jetpackpony@gmail.com>
2018-12-06 17:18:29 +0300

    Move Topbar into it's own folder

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -4,7 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Menu from './Menu';
+import Menu from '../Menu';
 import classes from './Topbar.module.scss';
 
 class Topbar extends Component {
